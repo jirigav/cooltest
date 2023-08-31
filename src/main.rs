d7a0a921dc708392cf39f434b6dd61c5e0e765c0
jirigav <jirigavenda98@gmail.com>
2023-08-31 13:52:40 +0200

    improve best final pattern using histogram

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ mod distinguishers;
 mod polyup;
 
 use crate::bottomup::bottomup;
-use crate::common::{load_data, shuffle_data, Args, Subcommands};
+use crate::common::{bits_block_eval, load_data, shuffle_data, z_score, Args, Subcommands};
 use crate::distinguishers::{
     best_multi_pattern, evaluate_distinguisher, Distinguisher, MultiPattern, Pattern,
 };
 use crate::polyup::polyup;
 use clap::Parser;
+use itertools::Itertools;
 use pyo3::prelude::*;
+use std::collections::HashSet;
 use std::time::Instant;
 
 fn p_value(positive: usize, sample_size: usize, probability: f64) -> f64 {
@@ -58,8 +60,8 @@ fn results(
     mut final_patterns: Vec<Pattern>,
     start: Instant,
     training_data: &[Vec<u8>],
-    testing_data_option: Option<Vec<Vec<u8>>>,
-    validation_data_option: Option<Vec<Vec<u8>>>,
+    testing_data_option: Option<&Vec<Vec<u8>>>,
+    validation_data_option: Option<&Vec<Vec<u8>>>,
     patterns_combined: usize,
 ) {
     final_patterns.sort_by(|a, b| {
@@ -69,7 +71,7 @@ fn results(
     });
     let mut b_multi_pattern: MultiPattern;
     if let Some(validation_data) = validation_data_option {
-        b_multi_pattern = best_multi_pattern(&validation_data, &final_patterns, patterns_combined);
+        b_multi_pattern = best_multi_pattern(validation_data, &final_patterns, patterns_combined);
     } else {
         b_multi_pattern = best_multi_pattern(training_data, &final_patterns, patterns_combined);
     }
@@ -82,7 +84,7 @@ fn results(
     if let Some(testing_data) = testing_data_option {
         println!(
             "z-score: {}",
-            evaluate_distinguisher(&mut b_multi_pattern, &testing_data)
+            evaluate_distinguisher(&mut b_multi_pattern, testing_data)
         );
         println!(
             "p-value: {:.0e}",
@@ -91,6 +93,80 @@ fn results(
                 testing_data.len(),
                 b_multi_pattern.probability
             )
+        );
+    }
+}
+
+fn get_hist(bits: &Vec<usize>, data: &[Vec<u8>]) -> Vec<usize> {
+    let mut hist = vec![0; 2_usize.pow(bits.len() as u32)];
+    for block in data {
+        hist[bits_block_eval(bits, block)] += 1;
+    }
+    hist
+}
+fn results2(
+    mut final_patterns: Vec<Pattern>,
+    start: Instant,
+    training_data: &[Vec<u8>],
+    testing_data_option: Option<&Vec<Vec<u8>>>,
+) {
+    println!("\n-- histograms --\n");
+    final_patterns.sort_by(|a, b| {
+        f64::abs(b.z_score.unwrap())
+            .partial_cmp(&f64::abs(a.z_score.unwrap()))
+            .unwrap()
+    });
+    let mut bits = HashSet::new();
+
+    for p in final_patterns.iter().take(1) {
+        bits.extend(p.bits.clone());
+    }
+
+    let mut bits_vec: Vec<usize> = bits.into_iter().collect();
+    bits_vec.sort();
+
+    println!("number of bits: {}", bits_vec.len());
+    if bits_vec.len() > 16 {
+        panic!();
+    }
+
+    let hist = get_hist(&bits_vec, training_data);
+
+    let mut indices = (0..2_usize.pow(bits_vec.len() as u32)).collect_vec();
+    indices.sort_by(|a, b| hist[*b].cmp(&hist[*a]));
+
+    let mut max_z = 0.0;
+    let mut best_i = 0;
+    let prob = 2.0_f64.powf(-(bits_vec.len() as f64));
+
+    for i in 1..2_usize.pow(bits_vec.len() as u32) {
+        let mut count = 0;
+        for k in 0..i {
+            count += hist[indices[k]];
+        }
+        let z = z_score(training_data.len(), count, prob * (i as f64));
+        if z > max_z {
+            max_z = z;
+            best_i = i;
+        }
+    }
+    println!("trained in {:.2?}", start.elapsed());
+
+    println!("z-score: {}", max_z);
+
+    if let Some(testing_data) = testing_data_option {
+        let test_hist = get_hist(&bits_vec, testing_data);
+
+        let mut count = 0;
+        for k in 0..best_i {
+            count += test_hist[indices[k]];
+        }
+        let z = z_score(testing_data.len(), count, prob * (best_i as f64));
+
+        println!("z-score: {}", z);
+        println!(
+            "p-value: {:.0e}",
+            p_value(count, testing_data.len(), prob * (best_i as f64))
         );
     }
 }
@@ -117,12 +193,18 @@ fn run_bottomup(
         base_pattern_size,
     );
     results(
+        final_patterns.clone(),
+        start,
+        &training_data,
+        testing_data_option.as_ref(),
+        validation_data_option.as_ref(),
+        patterns_combined,
+    );
+    results2(
         final_patterns,
         start,
         &training_data,
-        testing_data_option,
-        validation_data_option,
-        patterns_combined,
+        testing_data_option.as_ref(),
     );
 }
 
@@ -146,7 +228,7 @@ fn run_polyup(
     });
 
     //println!("{final_patterns:?}");
-    println!("{:?}", final_patterns[0].monomials);
+    println!("{:?}", final_patterns[0]);
     println!("{}", final_patterns[0].z_score.unwrap());
 
     if let Some(testing_data) = testing_data_option {
