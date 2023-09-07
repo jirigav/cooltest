f4a78d258fda563fc19a553b0860aa27762fa463
jirigav <jirigavenda98@gmail.com>
2023-09-07 20:43:55 +0200

    always use histograms, delete unnecessary stuff

--- src/bottomup.rs
+++ src/bottomup.rs
@@ -1,181 +1,118 @@
 use std::time::Instant;
 
-use crate::common::{bit_value_in_block, bits_block_eval, z_score};
-use crate::distinguishers::{Distinguisher, Pattern};
 use itertools::Itertools;
-use rayon::prelude::*;
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator, IndexedParallelIterator};
 
-fn phase_one(data: &[Vec<u8>], k: usize, block_size: usize, base_degree: usize) -> Vec<Pattern> {
-    let m = (0..2_u8.pow(base_degree as u32))
-        .map(|x| {
-            (0..base_degree)
-                .map(|i| bit_value_in_block(i, &[x]))
-                .collect_vec()
-        })
-        .collect_vec();
-    let mut best_patterns = vec![(0, (Vec::new(), Vec::new())); k];
+use crate::common::{bits_block_eval, z_score};
 
-    for bits in (0..block_size).combinations(base_degree) {
-        let hist = data
-            .par_iter()
-            .map(|block| bits_block_eval(&bits, block))
-            .fold_with(vec![0; 2_usize.pow(base_degree as u32)], |mut a, b| {
-                a[b] += 1;
-                a
-            })
-            .reduce(
-                || vec![0; 2_usize.pow(base_degree as u32)],
-                |mut a: Vec<usize>, b: Vec<usize>| {
-                    a = a.iter_mut().zip(b).map(|(a, b)| *a + b).collect();
-                    a
-                },
-            );
-
-        let max_count = hist.iter().max().unwrap();
-
-        if max_count > &best_patterns[k - 1].0 {
-            best_patterns[k - 1] = (
-                *max_count,
-                (
-                    bits,
-                    m[hist.iter().position(|x| x == max_count).unwrap()].clone(),
-                ),
-            );
-            best_patterns.sort_unstable_by(|a, b| b.0.cmp(&a.0));
-        }
-    }
-
-    best_patterns
-        .into_iter()
-        .map(|(count, (bits, values))| Pattern {
-            length: base_degree,
-            bits,
-            values,
-            count: Some(count),
-            z_score: None,
-        })
-        .collect()
+#[derive(Debug, Clone)]
+pub(crate) struct Histogram {
+    pub(crate) bits: Vec<usize>,
+    pub(crate) _bins: Vec<usize>,
+    pub(crate) sorted_indices: Vec<usize>,
+    pub(crate) best_division: usize,
+    pub(crate) z_score: f64
 }
 
-fn is_improving(old_z_score: f64, count_new: usize, new_length: usize, samples: usize) -> bool {
-    let p_new = 2_f64.powf(-(new_length as f64));
-    old_z_score <= z_score(samples, count_new, p_new)
-}
-
-fn improving(
-    pattern: &mut Pattern,
-    hist: &[(usize, usize)],
-    samples: usize,
-    min_difference: usize,
-) -> Vec<Pattern> {
-    let mut new_patterns = Vec::new();
-    if pattern.z_score.is_none() {
-        pattern.z_score(samples);
-    }
-    for (i, counts) in hist.iter().enumerate() {
-        if pattern.bits.contains(&i) {
-            continue;
-        }
-
-        let mut count = counts.0;
-        let mut v = false;
-        if counts.0 < counts.1 {
-            count = counts.1;
-            v = true;
-        }
-
-        if is_improving(pattern.z_score.unwrap(), count, pattern.length + 1, samples)
-            && count - ((2.0_f64.powf(-(pattern.length as f64 + 1.0)) * (samples as f64)) as usize)
-                >= min_difference
-        {
-            let mut new_pattern = pattern.clone();
-            new_pattern.forget_count();
-            new_pattern.add_bit(i, v);
-            new_pattern.increase_count(count);
-            new_patterns.push(new_pattern);
-        }
-    }
-    new_patterns
-}
-
-fn phase_two(
-    k: usize,
-    mut top_k: Vec<Pattern>,
-    data: &[Vec<u8>],
-    min_difference: usize,
-    block_size: usize,
-) -> Vec<Pattern> {
-    let mut final_patterns: Vec<Pattern> = Vec::with_capacity(k);
-
-    let mut pattern_len = 2;
-
-    while !top_k.is_empty() && pattern_len < block_size {
-        pattern_len += 1;
-
-        let mut hists: Vec<Vec<(usize, usize)>> = Vec::with_capacity(top_k.len());
-        for _ in 0..top_k.len() {
-            hists.push(vec![(0, 0); block_size]);
-        }
-
+impl Histogram {
+    pub(crate) fn get_hist(bits: &Vec<usize>, data: &[Vec<u8>]) -> Histogram {
+        let mut hist = vec![0; 2_usize.pow(bits.len() as u32)];
         for block in data {
-            for (i, pattern) in top_k.iter().enumerate() {
-                if pattern.evaluate(block) {
-                    for b in 0..block_size {
-                        if pattern.bits.contains(&b) {
-                            continue;
-                        }
-                        if bit_value_in_block(b, block) {
-                            hists[i][b].1 += 1;
-                        } else {
-                            hists[i][b].0 += 1;
-                        }
-                    }
-                }
+            hist[bits_block_eval(bits, block)] += 1;
+        }
+    
+        let mut indices = (0..2_usize.pow(bits.len() as u32)).collect_vec();
+        indices.sort_by(|a, b| hist[*b].cmp(&hist[*a]));
+    
+        let mut max_z = 0.0;
+        let mut best_i = 0;
+        let prob = 2.0_f64.powf(-(bits.len() as f64));
+    
+        for i in 1..2_usize.pow(bits.len() as u32) {
+            let mut count = 0;
+            for k in 0..i {
+                count += hist[indices[k]];
+            }
+            let z = z_score(data.len(), count, prob * (i as f64));
+            if z > max_z {
+                max_z = z;
+                best_i = i;
             }
         }
-
-        let mut new_top_k: Vec<Pattern> = Vec::with_capacity(top_k.len());
-
-        for i in 0..hists.len() {
-            let mut imp = improving(&mut top_k[i], &hists[i], data.len(), min_difference);
-
-            if imp.is_empty() {
-                final_patterns.push(top_k[i].clone());
-                continue;
-            }
-
-            imp.sort_unstable_by_key(|b| std::cmp::Reverse(b.get_count()));
-
-            let mut pattern_added = false;
-            for p in &imp {
-                if !new_top_k.contains(p) {
-                    new_top_k.push(p.clone());
-                    pattern_added = true;
-                    break;
-                }
-            }
-            if !pattern_added {
-                final_patterns.push(top_k[i].clone());
-            }
-        }
-        top_k = new_top_k;
+        Histogram { bits: bits.iter().copied().collect(), _bins: hist, sorted_indices: indices, best_division: best_i, z_score: max_z }
     }
-
-    final_patterns
 }
+
+
+
+fn phase_one(data: &[Vec<u8>], block_size: usize, deg: usize, k: usize) -> Vec<Histogram>{
+    let mut top_k : Vec<Histogram> = Vec::new();
+    for bits in (0..block_size).into_iter().combinations(deg) {
+        let hist = Histogram::get_hist(&bits, data);
+        if top_k.len() < k {
+            top_k.push(hist);
+        } else if top_k[0].z_score < hist.z_score{
+            top_k[0] = hist;
+        }
+        top_k.sort_unstable_by(|a, b| f64::abs(a.z_score).partial_cmp(&f64::abs(b.z_score)).unwrap());
+    }
+    top_k
+}
+
+fn phase_two(data: &[Vec<u8>], block_size: usize, mut top_k: Vec<Histogram>) -> Vec<Histogram> {
+    let mut final_bins: Vec<Histogram> = Vec::new();
+    let mut length = top_k[0].bits.len();
+    while !top_k.is_empty() && length < 10 {
+        length += 1;
+        let mut new_top: Vec<Histogram> = Vec::new();
+
+        for (i, hist) in top_k.par_iter().map(|hist| {
+            let mut best_imp: Option<Histogram> = None;
+            for bit in 0..block_size{
+                if hist.bits.contains(&bit){
+                    continue;
+                }
+                let mut new_bits = hist.bits.clone();
+                new_bits.push(bit);
+                let new_hist = Histogram::get_hist(&new_bits.iter().copied().collect(), data);
+                if new_hist.z_score < hist.z_score{
+                    continue;
+                }
+
+                if best_imp.is_none() || f64::abs(best_imp.as_ref().unwrap().z_score) <  f64::abs(new_hist.z_score){
+                    best_imp = Some(new_hist);
+                }
+
+            }
+            best_imp
+        }).enumerate().collect::<Vec<_>>() {
+            if let Some(imp) = hist {
+                new_top.push(imp);
+            } else {
+                final_bins.push(top_k[i].clone());
+            }
+        }
+        top_k = new_top;
+    }
+    final_bins.extend(top_k.into_iter());
+    final_bins
+} 
+
 
 pub(crate) fn bottomup(
     data: &[Vec<u8>],
     block_size: usize,
     k: usize,
-    min_difference: usize,
     base_degree: usize,
-) -> Vec<Pattern> {
+) -> Histogram {
     let mut start = Instant::now();
-    let top_k = phase_one(data, k, block_size, base_degree);
-    println!("phase one {:.2?}", start.elapsed());
+    let top_k = phase_one(data, block_size, base_degree, k);
+    println!("Phase one in {:?}", start.elapsed());
     start = Instant::now();
-    let r = phase_two(k, top_k, data, min_difference, block_size);
-    println!("phase two {:.2?}", start.elapsed());
-    r
+    let mut r = phase_two(data, block_size, top_k);
+    println!("Phase two in {:?}", start.elapsed());
+    r.sort_unstable_by(|a, b| f64::abs(b.z_score).partial_cmp(&f64::abs(a.z_score)).unwrap());
+    println!("{:?}", r[0]);
+    r[0].clone()
 }
+
--- src/main.rs
+++ src/main.rs
@@ -1,246 +1,63 @@
 mod bottomup;
 mod common;
-mod distinguishers;
-mod polyup;
 
 use crate::bottomup::bottomup;
-use crate::common::{
-    bits_block_eval, p_value, shuffle_data, z_score, Args, Subcommands,
-};
-use crate::distinguishers::{
-    best_multi_pattern, evaluate_distinguisher, Distinguisher, MultiPattern, Pattern,
-};
-use crate::polyup::polyup;
+use crate::common::{ p_value, z_score, Args};
 use clap::Parser;
 use common::prepare_data;
-use itertools::Itertools;
-use std::collections::HashSet;
+
 use std::time::Instant;
+use bottomup::Histogram;
 
-fn results(
-    mut final_patterns: Vec<Pattern>,
-    start: Instant,
-    training_data: &[Vec<u8>],
-    testing_data_option: Option<&Vec<Vec<u8>>>,
-    validation_data_option: Option<&Vec<Vec<u8>>>,
-    patterns_combined: usize,
-    hist: bool,
-) {
-    final_patterns.sort_by(|a, b| {
-        f64::abs(b.z_score.unwrap())
-            .partial_cmp(&f64::abs(a.z_score.unwrap()))
-            .unwrap()
-    });
-    let mut b_multi_pattern: MultiPattern;
-    if let Some(validation_data) = validation_data_option {
-        b_multi_pattern = best_multi_pattern(validation_data, &final_patterns, patterns_combined);
-    } else {
-        b_multi_pattern = best_multi_pattern(training_data, &final_patterns, patterns_combined);
-    }
-
-    println!("trained in {:.2?}", start.elapsed());
-
-    println!("z-score: {}", b_multi_pattern.z_score.unwrap());
-    println!("best multi-pattern: {b_multi_pattern:?}");
-
-    if let Some(testing_data) = testing_data_option {
-        println!(
-            "z-score: {}",
-            evaluate_distinguisher(&mut b_multi_pattern, testing_data)
-        );
-        println!(
-            "p-value: {:.0e}",
-            p_value(
-                b_multi_pattern.get_count(),
-                testing_data.len(),
-                b_multi_pattern.probability
-            )
-        );
-    }
-    if hist {
-        hist_result(final_patterns, training_data, testing_data_option);
-    }
-}
-
-fn get_hist(bits: &Vec<usize>, data: &[Vec<u8>]) -> Vec<usize> {
-    let mut hist = vec![0; 2_usize.pow(bits.len() as u32)];
-    for block in data {
-        hist[bits_block_eval(bits, block)] += 1;
-    }
-    hist
-}
-fn hist_result(
-    final_patterns: Vec<Pattern>,
-    training_data: &[Vec<u8>],
-    testing_data_option: Option<&Vec<Vec<u8>>>,
-) {
-    println!("\n-- histograms --\n");
-    let mut bits = HashSet::new();
-
-    for p in final_patterns.iter().take(1) {
-        bits.extend(p.bits.clone());
-    }
-
-    let mut bits_vec: Vec<usize> = bits.into_iter().collect();
-    bits_vec.sort();
-
-    println!("number of bits: {}", bits_vec.len());
-
-    if bits_vec.len() > 16 {
-        println!("Too many bits in pattern, can't produce hist result.");
-        return;
-    }
-
-    let hist = get_hist(&bits_vec, training_data);
-
-    let mut indices = (0..2_usize.pow(bits_vec.len() as u32)).collect_vec();
-    indices.sort_by(|a, b| hist[*b].cmp(&hist[*a]));
-
-    let mut max_z = 0.0;
-    let mut best_i = 0;
-    let prob = 2.0_f64.powf(-(bits_vec.len() as f64));
-
-    for i in 1..2_usize.pow(bits_vec.len() as u32) {
-        let mut count = 0;
-        for k in 0..i {
-            count += hist[indices[k]];
-        }
-        let z = z_score(training_data.len(), count, prob * (i as f64));
-        if z > max_z {
-            max_z = z;
-            best_i = i;
-        }
-    }
-
-    println!("z-score: {}", max_z);
-
-    if let Some(testing_data) = testing_data_option {
-        let test_hist = get_hist(&bits_vec, testing_data);
-
-        let mut count = 0;
-        for k in 0..best_i {
-            count += test_hist[indices[k]];
-        }
-        let z = z_score(testing_data.len(), count, prob * (best_i as f64));
-
-        println!("z-score: {}", z);
-        println!(
-            "p-value: {:.0e}",
-            p_value(count, testing_data.len(), prob * (best_i as f64))
-        );
-    }
-}
 
 fn run_bottomup(
     data_source: &str,
     block_size: usize,
     k: usize,
-    min_difference: usize,
-    patterns_combined: usize,
     base_pattern_size: usize,
     halving: bool,
     validation: bool,
-    hist: bool,
 ) {
-    let (training_data, validation_data_option, testing_data_option) =
+    let (training_data, _validation_data_option, testing_data_option) =
         prepare_data(data_source, block_size, halving, validation);
 
     let start = Instant::now();
-    let final_patterns = bottomup(
+    let hist = bottomup(
         &training_data,
         block_size,
         k,
-        min_difference,
         base_pattern_size,
     );
-    results(
-        final_patterns.clone(),
-        start,
-        &training_data,
-        testing_data_option.as_ref(),
-        validation_data_option.as_ref(),
-        patterns_combined,
-        hist,
-    );
-}
+    println!("training finished in {:?}", start.elapsed());
+    let testing_data = testing_data_option.unwrap();
+    let test_hist = Histogram::get_hist(&hist.bits, &testing_data);
 
-fn run_polyup(
-    data_source: &str,
-    block_size: usize,
-    k: usize,
-    n: usize,
-    min_difference: usize,
-    halving: bool,
-) {
-    let (training_data, _validation_data_option, testing_data_option) =
-        prepare_data(data_source, block_size, halving, false);
-
-    let _start = Instant::now();
-    let mut final_patterns = polyup(&training_data, block_size, n, k, min_difference);
-    final_patterns.sort_by(|a, b| {
-        f64::abs(b.z_score.unwrap())
-            .partial_cmp(&f64::abs(a.z_score.unwrap()))
-            .unwrap()
-    });
-
-    //println!("{final_patterns:?}");
-    println!("{:?}", final_patterns[0]);
-    println!("{}", final_patterns[0].z_score.unwrap());
-
-    if let Some(testing_data) = testing_data_option {
-        println!(
-            "z-score: {}",
-            evaluate_distinguisher(&mut final_patterns[0], &testing_data)
-        );
-        println!(
-            "p-value: {:.0e}",
-            p_value(
-                final_patterns[0].get_count(),
-                testing_data.len(),
-                final_patterns[0].probability
-            )
-        );
+    let mut count = 0;
+    for k in 0..hist.best_division {
+        count += test_hist._bins[hist.sorted_indices[k]];
     }
+    let prob = 2.0_f64.powf(-(hist.bits.len() as f64));
+    let z = z_score(testing_data.len(), count, prob * (hist.best_division as f64));
+
+    println!("z-score: {}", z);
+    println!(
+        "p-value: {:.0e}",
+        p_value(count, testing_data.len(), prob * (hist.best_division as f64))
+    );
+
 }
 
 fn main() {
     let args = Args::parse();
     println!("\n{args:?}\n");
 
-    match args.tool {
-        Subcommands::ShuffleData {
-            block_size,
-            input_file_path,
-            output_file_path,
-        } => shuffle_data(&input_file_path, &output_file_path, block_size),
-        Subcommands::Bottomup {
-            data_source,
-            block_size,
-            k,
-            min_difference,
-            patterns_combined,
-            base_pattern_size,
-            halving,
-            validation_and_testing_split,
-            hist,
-        } => run_bottomup(
-            &data_source,
-            block_size,
-            k,
-            min_difference,
-            patterns_combined,
-            base_pattern_size,
-            halving,
-            validation_and_testing_split,
-            hist,
-        ),
-        Subcommands::Polyup {
-            data_source,
-            block_size,
-            k,
-            n,
-            min_difference,
-            halving,
-        } => run_polyup(&data_source, block_size, k, n, min_difference, halving),
+    run_bottomup(
+            &args.data_source,
+            args.block_size,
+            args.k,
+            args.base_pattern_size,
+            args.halving,
+            args.validation_and_testing_split,
+        )
     }
-}
+
