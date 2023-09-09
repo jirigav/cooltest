6bb0614a4a0c550179e55912d94492785ae96e4a
jirigav <jirigavenda98@gmail.com>
2023-09-09 23:54:07 +0200

    add alternative phase2

--- src/bottomup.rs
+++ src/bottomup.rs
@@ -90,6 +90,7 @@ fn improving(
             let mut new_pattern = pattern.clone();
             new_pattern.forget_count();
             new_pattern.add_bit(i, v);
+            println!("{}   {} - {:?}", imp_p_value, p_value(count, pattern.get_count(), 0.5), new_pattern.bits);
             new_pattern.increase_count(count);
             new_patterns.push(new_pattern);
         }
@@ -101,17 +102,18 @@ fn phase_two(
     k: usize,
     mut top_k: Vec<Pattern>,
     data: &[Vec<u8>],
-    imp_p_value: f64,
+    mut imp_p_value: f64,
     block_size: usize,
     base_degree: usize
 ) -> Vec<Pattern> {
     let mut final_patterns: Vec<Pattern> = Vec::with_capacity(k);
 
     let mut pattern_len = base_degree;
-
+    //imp_p_value *= 0.5_f64.powf(base_degree as f64);
 
     while !top_k.is_empty() && pattern_len < block_size {
         pattern_len += 1;
+        imp_p_value *= 0.6;
 
 
         let mut hists: Vec<Vec<(usize, usize)>> = Vec::with_capacity(top_k.len());
@@ -174,4 +176,79 @@ pub(crate) fn bottomup(data: &[Vec<u8>], args: &Args) -> Vec<Pattern> {
     let r = phase_two(args.k, top_k, data, args.improving_p_value, args.block_size, args.base_pattern_size);
     println!("phase two {:.2?}", start.elapsed());
     r
+}
+
+
+fn phase_two_alt(
+    k: usize,
+    mut top_k: Vec<Pattern>,
+    data: &[Vec<u8>],
+    imp_p_value: f64,
+    block_size: usize,
+    base_degree: usize
+) -> Vec<Pattern> {
+    let mut final_patterns: Vec<Pattern> = Vec::with_capacity(k);
+
+    let mut pattern_len = base_degree;
+
+    while !top_k.is_empty() && pattern_len < block_size {
+        pattern_len += 1;
+
+        let mut hists: Vec<Vec<(usize, usize)>> = Vec::with_capacity(top_k.len());
+        for _ in 0..top_k.len() {
+            hists.push(vec![(0, 0); block_size]);
+        }
+
+        for block in data {
+            for (i, pattern) in top_k.iter().enumerate() {
+                if pattern.evaluate(block) {
+                    for b in 0..block_size {
+                        if pattern.bits.contains(&b) {
+                            continue;
+                        }
+                        if bit_value_in_block(b, block) {
+                            hists[i][b].1 += 1;
+                        } else {
+                            hists[i][b].0 += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        let mut new_top_k: Vec<Pattern> = Vec::with_capacity(top_k.len());
+
+        for i in 0..hists.len() {
+            let mut imp = improving(&mut top_k[i], &hists[i], data.len(), imp_p_value);
+
+            if imp.is_empty() {
+                final_patterns.push(top_k[i].clone());
+                continue;
+            }
+
+            imp.sort_unstable_by_key(|b| std::cmp::Reverse(b.get_count()));
+
+            let mut pattern_added = false;
+            for p in &imp {
+                if !new_top_k.contains(p) {
+                    if new_top_k.len() < k{
+                        new_top_k.push(p.clone());
+                    }else if new_top_k[0].get_count() < p.get_count(){
+                        pattern_added = true;
+                        new_top_k[0] = p.clone();
+                        new_top_k.sort_unstable_by_key(|pattern| pattern.get_count());
+
+                    } else {
+                        break;
+                    }
+                }
+            }
+            if !pattern_added {
+                final_patterns.push(top_k[i].clone());
+            }
+        }
+        top_k = new_top_k;
+    }
+
+    final_patterns
 }
