62442108f30b77ed01fe47ea1ea7eb9c04e1be31
jirigav <jirigavenda98@gmail.com>
2023-09-18 17:13:09 +0200

    make phase one faster

--- src/bottomup.rs
+++ src/bottomup.rs
@@ -1,9 +1,11 @@
 use std::time::Instant;
 
 use itertools::Itertools;
-use rayon::prelude::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
+use rayon::prelude::*;
 
-use crate::common::{bits_block_eval, z_score};
+use crate::common::{
+    bits_block_eval, count_combinations, multi_eval_count, transform_data, z_score, Data,
+};
 
 #[derive(Debug, Clone)]
 pub(crate) struct Histogram {
@@ -48,6 +50,34 @@ impl Histogram {
         }
     }
 
+    pub(crate) fn from_bins(bits: Vec<usize>, bins: Vec<usize>) -> Histogram {
+        let mut indices = (0..2_usize.pow(bits.len() as u32)).collect_vec();
+        indices.sort_by(|a, b| bins[*b].cmp(&bins[*a]));
+
+        let mut max_z = 0.0;
+        let mut best_i = 0;
+        let prob = 2.0_f64.powf(-(bits.len() as f64));
+
+        for i in 1..2_usize.pow(bits.len() as u32) {
+            let mut count = 0;
+            for k in 0..i {
+                count += bins[indices[k]];
+            }
+            let z = z_score(bins.iter().sum(), count, prob * (i as f64));
+            if z > max_z {
+                max_z = z;
+                best_i = i;
+            }
+        }
+        Histogram {
+            bits: bits.to_vec(),
+            _bins: bins,
+            sorted_indices: indices,
+            best_division: best_i,
+            z_score: max_z,
+        }
+    }
+
     pub(crate) fn evaluate(&self, data: &[Vec<u8>]) -> usize {
         let mut hist2 = vec![0; 2_usize.pow(self.bits.len() as u32)];
         for block in data {
@@ -61,22 +91,33 @@ impl Histogram {
     }
 }
 
-fn phase_one(data: &[Vec<u8>], block_size: usize, deg: usize, k: usize) -> Vec<Histogram> {
-    let mut top_k: Vec<Histogram> = Vec::new();
-    for bits in (0..block_size).combinations(deg) {
-        let hist = Histogram::get_hist(&bits, data);
-        if top_k.len() < k {
-            top_k.push(hist);
-        } else if top_k[0].z_score < hist.z_score {
-            top_k[0] = hist;
-        }
-        top_k.sort_unstable_by(|a, b| {
-            f64::abs(a.z_score)
-                .partial_cmp(&f64::abs(b.z_score))
-                .unwrap()
-        });
+fn phase_one(data: &Data, block_size: usize, base_degree: usize, k: usize) -> Vec<Histogram> {
+    let dises_per_bits = 2_usize.pow(base_degree as u32);
+    let num_of_dises = count_combinations(block_size, base_degree) * dises_per_bits;
+    let mut counts: Vec<usize> = vec![0; num_of_dises];
+    let bits = (0..block_size).combinations(base_degree).collect_vec();
+
+    let mut it = data.data.iter().peekable();
+    while let Some(blocks) = it.next() {
+        let is_last = it.peek().is_none();
+
+        counts.par_iter_mut().enumerate().for_each(|(i, c)| {
+            let bits_index = i / dises_per_bits;
+            let bits_signs = i % dises_per_bits;
+            *c += multi_eval_count(bits_signs, &bits[bits_index], blocks, data.mask, is_last)
+                as usize;
+        })
     }
-    top_k
+
+    let mut best: Vec<_> = counts
+        .into_par_iter()
+        .chunks(dises_per_bits)
+        .enumerate()
+        .map(|(i, bins)| Histogram::from_bins(bits[i].clone(), bins))
+        .collect();
+
+    best.sort_by(|a, b| b.z_score.partial_cmp(&a.z_score).unwrap());
+    best.into_iter().take(k).collect()
 }
 
 fn phase_two(
@@ -130,14 +171,14 @@ fn phase_two(
 }
 
 pub(crate) fn bottomup(
-    data: &[Vec<u8>],
+    data: &Vec<Vec<u8>>,
     block_size: usize,
     k: usize,
     base_degree: usize,
     max_bits: usize,
 ) -> Histogram {
     let mut start = Instant::now();
-    let top_k = phase_one(data, block_size, base_degree, k);
+    let top_k = phase_one(&transform_data(data), block_size, base_degree, k);
     println!("Phase one in {:?}", start.elapsed());
     start = Instant::now();
     let mut r = phase_two(data, block_size, top_k, max_bits);
--- src/common.rs
+++ src/common.rs
@@ -36,11 +36,6 @@ pub(crate) struct Args {
     pub(crate) max_bits: usize,
 }
 
-pub(crate) fn bit_value_in_block(bit: usize, block: &[u8]) -> bool {
-    let (byte_index, offset) = (bit / 8, bit % 8);
-    ((block[byte_index] >> offset) & 1) == 1
-}
-
 pub(crate) fn bits_block_eval(bits: &[usize], block: &[u8]) -> usize {
     let mut result = 0;
 
@@ -50,6 +45,57 @@ pub(crate) fn bits_block_eval(bits: &[usize], block: &[u8]) -> usize {
         }
     }
     result
+}
+
+pub(crate) fn bit_value_in_block(bit: usize, block: &[u8]) -> bool {
+    let (byte_index, offset) = (bit / 8, bit % 8);
+    ((block[byte_index] >> offset) & 1) == 1
+}
+
+pub(crate) fn count_combinations(n: usize, r: usize) -> usize {
+    if r > n {
+        0
+    } else {
+        (1..=r.min(n - r)).fold(1, |acc, val| acc * (n - val + 1) / val)
+    }
+}
+
+pub(crate) struct Data {
+    pub(crate) data: Vec<Vec<u128>>,
+    pub(crate) mask: u128,
+    pub(crate) _num_of_blocks: usize,
+}
+
+pub(crate) fn multi_eval(
+    bits_signs: usize,
+    bits: &[usize],
+    tr_data: &[u128],
+    mask: u128,
+    is_last: bool,
+) -> u128 {
+    let mut result = u128::MAX;
+
+    for (i, b) in bits.iter().enumerate() {
+        if ((bits_signs >> i) & 1) == 1 {
+            result &= tr_data[*b];
+        } else {
+            result &= tr_data[*b] ^ u128::MAX;
+        }
+    }
+    if is_last {
+        result &= mask;
+    }
+    result
+}
+
+pub(crate) fn multi_eval_count(
+    bits_signs: usize,
+    bits: &[usize],
+    tr_data: &[u128],
+    mask: u128,
+    is_last: bool,
+) -> u32 {
+    multi_eval(bits_signs, bits, tr_data, mask, is_last).count_ones()
 }
 
 fn load_data(path: &str, block_size: usize) -> Vec<Vec<u8>> {
@@ -67,22 +113,53 @@ pub(crate) fn prepare_data(
     halving: bool,
     validation: bool,
 ) -> (Vec<Vec<u8>>, Option<Vec<Vec<u8>>>, Option<Vec<Vec<u8>>>) {
-    let mut training_data = load_data(data_source, block_size);
+    let data = load_data(data_source, block_size);
+    let training_data;
     let mut testing_data_option = None;
     let mut validation_data_option = None;
 
     if validation {
-        let (tr_data, testing_data) = training_data.split_at(training_data.len() / 3);
+        let (tr_data, testing_data) = data.split_at(data.len() / 3);
         let (val_data, test_data) = testing_data.split_at(testing_data.len() / 2);
         testing_data_option = Some(test_data.to_vec());
         validation_data_option = Some(val_data.to_vec());
         training_data = tr_data.to_vec();
     } else if halving {
-        let (tr_data, testing_data) = training_data.split_at(training_data.len() / 2);
+        let (tr_data, testing_data) = data.split_at(data.len() / 2);
         testing_data_option = Some(testing_data.to_vec());
         training_data = tr_data.to_vec();
+    } else {
+        training_data = data;
     }
     (training_data, validation_data_option, testing_data_option)
+}
+
+/// Returns data transformed into vectors of u64, where i-th u64 contains values of 64 i-th bits of consecutive blocks.
+pub(crate) fn transform_data(data: &Vec<Vec<u8>>) -> Data {
+    let mut result = Vec::new();
+    let block_size = data[0].len() * 8;
+    for blocks in data.chunks(128) {
+        let mut ints = vec![0_u128; block_size];
+
+        for (e, block) in blocks.iter().enumerate() {
+            for (i, int) in ints.iter_mut().enumerate().take(block_size) {
+                if bit_value_in_block(i, block) {
+                    *int += 1_u128 << e;
+                }
+            }
+        }
+        result.push(ints);
+    }
+    let mask = if data.len() % 128 == 0 {
+        u128::MAX
+    } else {
+        2_u128.pow((data.len() % 128) as u32) - 1
+    };
+    Data {
+        data: result,
+        mask,
+        _num_of_blocks: data.len(),
+    }
 }
 
 pub(crate) fn p_value(positive: usize, sample_size: usize, probability: f64) -> f64 {
