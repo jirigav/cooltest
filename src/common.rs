f07dac02672a61bc2219af15bbb3496369ddab6d
jirigav <jirigavenda98@gmail.com>
2023-11-27 17:30:02 +0100

    remove unnecessary code, improve args

--- src/common.rs
+++ src/common.rs
@@ -1,7 +1,5 @@
-use clap::{Parser, Subcommand};
-use rand::seq::SliceRandom;
-use std::fs::File;
-use std::{fs, io::Write};
+use clap::Parser;
+use std::fs;
 
 pub(crate) fn z_score(sample_size: usize, positive: usize, p: f64) -> f64 {
     ((positive as f64) - p * (sample_size as f64)) / f64::sqrt(p * (1.0 - p) * (sample_size as f64))
@@ -10,83 +8,28 @@ pub(crate) fn z_score(sample_size: usize, positive: usize, p: f64) -> f64 {
 #[derive(Parser)]
 #[command(version)]
 pub(crate) struct Args {
-    #[clap(subcommand)]
-    pub(crate) tool: Subcommands,
-}
+    /// Path of file with input data.
+    pub(crate) data_source: String,
 
-#[derive(Subcommand)]
-#[command(version)]
-pub(crate) enum Subcommands {
-    /// Simple tool to shuffle blocks of given length of the input file and save shuffled data into output file.
-    ShuffleData {
-        /// Length of block of data.
-        block_size: usize,
+    /// Length of block of data.
+    #[arg(short, long, default_value_t = 128)]
+    pub(crate) block_size: usize,
 
-        /// Path to input file.
-        input_file_path: String,
+    /// Number of explored pattern branches.
+    #[arg(short, long, default_value_t = 10)]
+    pub(crate) k: usize,
 
-        /// Path to where the optput of this function should be stored.
-        output_file_path: String,
-    },
+    /// Degree of distinguishers evaluated in the first phase.
+    #[arg(short, long, default_value_t = 2)]
+    pub(crate) deg: usize,
 
-    /// Tool for finding frequent patterns in data.
-    Bottomup {
-        /// Path of file with input data.
-        data_source: String,
+    /// Minimal difference between expected and actual count of a given pattern in data.
+    #[arg(short, long, default_value_t = 100)]
+    pub(crate) min_difference: usize,
 
-        /// Length of block of data.
-        #[arg(short, long, default_value_t = 128)]
-        block_size: usize,
-
-        /// Number of explored pattern branches.
-        #[arg(short, long, default_value_t = 10)]
-        k: usize,
-
-        /// Minimal difference between expected and actual count of a given pattern in data.
-        #[arg(short, long, default_value_t = 100)]
-        min_difference: usize,
-
-        /// Number of patterns combined into a multipattern.
-        #[arg(short, long, default_value_t = 2)]
-        patterns_combined: usize,
-
-        /// Length of patterns evaluated in the first phase.
-        #[arg(short, long, default_value_t = 2)]
-        base_pattern_size: usize,
-
-        /// Option whether the input data should be halved into training and testing data.
-        #[arg(long)]
-        halving: bool,
-
-        /// Option whether the input data should be divided into training, validation and testing data.
-        #[arg(long, short)]
-        validation_and_testing_split: bool,
-    },
-    /// Tool similar to bottom up with base_pattern_size=1, but with distinguishers constructed as boolean polynomials and the ability to find also infrequent patterns.
-    Polyup {
-        /// Path of file with input data.
-        data_source: String,
-
-        /// Length of block of data.
-        #[arg(short, long, default_value_t = 128)]
-        block_size: usize,
-
-        /// Number of explored pattern branches.
-        #[arg(short, long, default_value_t = 10)]
-        k: usize,
-
-        ///
-        #[arg(short, long, default_value_t = 2)]
-        n: usize,
-
-        /// Minimal difference between expected and actual count of a given pattern in data.
-        #[arg(short, long, default_value_t = 100)]
-        min_difference: usize,
-
-        /// Option whether the input data should be halved into training and testing data.
-        #[arg(long)]
-        halving: bool,
-    },
+    /// Significance level
+    #[arg(short, long, default_value_t = 0.0004)]
+    pub(crate) alpha: f64,
 }
 
 pub(crate) fn bit_value_in_block(bit: usize, block: &[u8]) -> bool {
@@ -112,16 +55,4 @@ pub(crate) fn load_data(path: &str, block_size: usize) -> Vec<Vec<u8>> {
         .chunks(len_of_block_in_bytes)
         .map(<[u8]>::to_vec)
         .collect()
-}
-
-pub(crate) fn shuffle_data(data_in_path: &str, data_out_path: &str, block_size: usize) {
-    let mut data = load_data(data_in_path, block_size);
-    let mut rng = rand::thread_rng();
-    data.shuffle(&mut rng);
-
-    let mut file_out = File::create(data_out_path).unwrap();
-
-    file_out
-        .write_all(&data.iter().flatten().copied().collect::<Vec<u8>>())
-        .unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
-mod bottomup;
 mod common;
 mod distinguishers;
 mod polyup;
 
-use crate::bottomup::bottomup;
-use crate::common::{load_data, shuffle_data, Args, Subcommands};
-use crate::distinguishers::{
-    best_multi_pattern, evaluate_distinguisher, Distinguisher, MultiPattern, Pattern,
-};
+use crate::common::{load_data, Args};
+use crate::distinguishers::{evaluate_distinguisher, Distinguisher};
 use crate::polyup::polyup;
 use clap::Parser;
 use pyo3::prelude::*;
 use std::time::Instant;
+
+fn print_results(p_value: f64, z_score: f64, alpha: f64) {
+    println!("---------------------------------------------------");
+    println!("z-score: {z_score}");
+    println!("p-value: {p_value:.0e}");
+    if p_value >= alpha {
+        println!(
+            "As the p-value >= alpha {alpha:.0e}, the randomness hypothesis cannot be rejected."
+        );
+        println!("= CoolTest could not find statistically significant non-randomness.");
+    } else {
+        println!("As the p-value < alpha {alpha:.0e}, the randomness hypothesis is REJECTED.");
+        println!("= Data is not random.");
+    }
+}
 
 fn p_value(positive: usize, sample_size: usize, probability: f64) -> f64 {
     pyo3::prepare_freethreaded_python();
@@ -30,100 +41,12 @@ fn p_value(positive: usize, sample_size: usize, probability: f64) -> f64 {
     })
 }
 
-fn prepare_data(
-    data_source: &str,
-    block_size: usize,
-    halving: bool,
-    validation: bool,
-) -> (Vec<Vec<u8>>, Option<Vec<Vec<u8>>>, Option<Vec<Vec<u8>>>) {
-    let mut training_data = load_data(data_source, block_size);
-    let mut testing_data_option = None;
-    let mut validation_data_option = None;
+fn prepare_data(data_source: &str, block_size: usize) -> (Vec<Vec<u8>>, Vec<Vec<u8>>) {
+    let training_data = load_data(data_source, block_size);
 
-    if validation {
-        let (tr_data, testing_data) = training_data.split_at(training_data.len() / 3);
-        let (val_data, test_data) = testing_data.split_at(testing_data.len() / 2);
-        testing_data_option = Some(test_data.to_vec());
-        validation_data_option = Some(val_data.to_vec());
-        training_data = tr_data.to_vec();
-    } else if halving {
-        let (tr_data, testing_data) = training_data.split_at(training_data.len() / 2);
-        testing_data_option = Some(testing_data.to_vec());
-        training_data = tr_data.to_vec();
-    }
-    (training_data, validation_data_option, testing_data_option)
-}
+    let (tr_data, testing_data) = training_data.split_at(training_data.len() / 2);
 
-fn results(
-    mut final_patterns: Vec<Pattern>,
-    start: Instant,
-    training_data: &[Vec<u8>],
-    testing_data_option: Option<Vec<Vec<u8>>>,
-    validation_data_option: Option<Vec<Vec<u8>>>,
-    patterns_combined: usize,
-) {
-    final_patterns.sort_by(|a, b| {
-        f64::abs(b.z_score.unwrap())
-            .partial_cmp(&f64::abs(a.z_score.unwrap()))
-            .unwrap()
-    });
-    let mut b_multi_pattern: MultiPattern;
-    if let Some(validation_data) = validation_data_option {
-        b_multi_pattern = best_multi_pattern(&validation_data, &final_patterns, patterns_combined);
-    } else {
-        b_multi_pattern = best_multi_pattern(training_data, &final_patterns, patterns_combined);
-    }
-
-    println!("trained in {:.2?}", start.elapsed());
-
-    println!("z-score: {}", b_multi_pattern.z_score.unwrap());
-    println!("best multi-pattern: {b_multi_pattern:?}");
-
-    if let Some(testing_data) = testing_data_option {
-        println!(
-            "z-score: {}",
-            evaluate_distinguisher(&mut b_multi_pattern, &testing_data)
-        );
-        println!(
-            "p-value: {:.0e}",
-            p_value(
-                b_multi_pattern.get_count(),
-                testing_data.len(),
-                b_multi_pattern.probability
-            )
-        );
-    }
-}
-
-fn run_bottomup(
-    data_source: &str,
-    block_size: usize,
-    k: usize,
-    min_difference: usize,
-    patterns_combined: usize,
-    base_pattern_size: usize,
-    halving: bool,
-    validation: bool,
-) {
-    let (training_data, validation_data_option, testing_data_option) =
-        prepare_data(data_source, block_size, halving, validation);
-
-    let start = Instant::now();
-    let final_patterns = bottomup(
-        &training_data,
-        block_size,
-        k,
-        min_difference,
-        base_pattern_size,
-    );
-    results(
-        final_patterns,
-        start,
-        &training_data,
-        testing_data_option,
-        validation_data_option,
-        patterns_combined,
-    );
+    (tr_data.to_vec(), testing_data.to_vec())
 }
 
 fn run_polyup(
@@ -132,10 +55,9 @@ fn run_polyup(
     k: usize,
     n: usize,
     min_difference: usize,
-    halving: bool,
+    alpha: f64,
 ) {
-    let (training_data, _validation_data_option, testing_data_option) =
-        prepare_data(data_source, block_size, halving, false);
+    let (training_data, testing_data) = prepare_data(data_source, block_size);
 
     let _start = Instant::now();
     let mut final_patterns = polyup(&training_data, block_size, n, k, min_difference);
@@ -149,57 +71,26 @@ fn run_polyup(
     println!("{:?}", final_patterns[0]);
     println!("{}", final_patterns[0].z_score.unwrap());
 
-    if let Some(testing_data) = testing_data_option {
-        println!(
-            "z-score: {}",
-            evaluate_distinguisher(&mut final_patterns[0], &testing_data)
-        );
-        println!(
-            "p-value: {:.0e}",
-            p_value(
-                final_patterns[0].get_count(),
-                testing_data.len(),
-                final_patterns[0].probability
-            )
-        );
-    }
+    print_results(
+        p_value(
+            final_patterns[0].get_count(),
+            testing_data.len(),
+            final_patterns[0].probability,
+        ),
+        evaluate_distinguisher(&mut final_patterns[0], &testing_data),
+        alpha,
+    )
 }
 
 fn main() {
     let args = Args::parse();
 
-    match args.tool {
-        Subcommands::ShuffleData {
-            block_size,
-            input_file_path,
-            output_file_path,
-        } => shuffle_data(&input_file_path, &output_file_path, block_size),
-        Subcommands::Bottomup {
-            data_source,
-            block_size,
-            k,
-            min_difference,
-            patterns_combined,
-            base_pattern_size,
-            halving,
-            validation_and_testing_split,
-        } => run_bottomup(
-            &data_source,
-            block_size,
-            k,
-            min_difference,
-            patterns_combined,
-            base_pattern_size,
-            halving,
-            validation_and_testing_split,
-        ),
-        Subcommands::Polyup {
-            data_source,
-            block_size,
-            k,
-            n,
-            min_difference,
-            halving,
-        } => run_polyup(&data_source, block_size, k, n, min_difference, halving),
-    }
+    run_polyup(
+        &args.data_source,
+        args.block_size,
+        args.k,
+        args.deg,
+        args.min_difference,
+        args.alpha,
+    )
 }
