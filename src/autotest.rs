dd8178238ffbdeb8e4b97b89ca17f8472f60a4fe
jirigav <jirigavenda98@gmail.com>
2024-06-26 14:58:26 +0200

    adjust alpha when multiple tests are run

--- src/autotest.rs
+++ src/autotest.rs
@@ -8,14 +8,16 @@ fn choose_k(_block_size: usize, _data_size: usize) -> usize {
     3
 }
 
-pub(crate) fn autotest(args: Args) {
+pub(crate) fn autotest(mut args: Args) {
     let (training_data, testing_data) = prepare_data(&args.data_source, args.block, true);
     let mut testing_data = testing_data.unwrap();
+    let mut tested_cases = 0;
     let start = Instant::now();
     let data_size = training_data.len();
 
     let mut k = choose_k(args.block, data_size);
 
+    tested_cases += 1;
     let mut hist = bottomup(
         &training_data,
         args.block,
@@ -26,6 +28,7 @@ pub(crate) fn autotest(args: Args) {
     );
     let testing_data2;
     if args.block <= 256 {
+        tested_cases += 1;
         let (training_data, testing_data_opt2) =
             prepare_data(&args.data_source, 2 * args.block, true);
         testing_data2 = testing_data_opt2.unwrap();
@@ -44,6 +47,12 @@ pub(crate) fn autotest(args: Args) {
         }
     }
     println!("training finished in {:?}", start.elapsed());
+
+    if tested_cases > 1 {
+        let new_alpha = args.alpha/(tested_cases as f64);
+        println!("Adjusting significance level based on the number of tests from {} to {}", args.alpha, new_alpha);
+        args.alpha = new_alpha;
+    }
 
     results(hist, &testing_data, args)
 }
